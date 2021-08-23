9ed03a9efbf6e7686bcfdd29e76a66dadec6a62f
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2021-08-23 15:42:02 +0900

    管理者のCRUD機能を実装

--- backend/app/Http/Controllers/Api/AdminUserController.php
+++ backend/app/Http/Controllers/Api/AdminUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\AdminUser;
 
 class AdminUserController extends Controller
 {
@@ -14,7 +16,7 @@ class AdminUserController extends Controller
      */
     public function index()
     {
-        //
+        return AdminUser::all();
     }
 
     /**
@@ -25,7 +27,11 @@ class AdminUserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $admin_user = new AdminUser;
+        $admin_user->fill(array_merge(
+            $request->all(), ['password' => Hash::make($request->password)]
+        ))->save();
+        return $admin_user;
     }
 
     /**
@@ -59,6 +65,8 @@ class AdminUserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $admin_user = AdminUser::find($id);
+        $admin_user->delete();
+        return $admin_user;
     }
 }
